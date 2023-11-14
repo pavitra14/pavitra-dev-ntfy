7733d837c737286d5250fb18412923c0c8cae8ac
Pavitra Behre <behrepav@amazon.com>
2023-11-14 17:46:11 +0530

    refactor(Config Class): add error handling and documentation
    - Add docstrings to Config class and its methods
    - Modify the 'get' method to use 'dict.get' for safe key retrieval

--- controllers/config.py
+++ controllers/config.py
@@ -1,12 +1,25 @@
 from dotenv import dotenv_values, load_dotenv
 
 class Config:
+    """
+    Class for loading and retrieving configuration values from .env file.
+    """
     def __init__(self) -> None:
+        """
+        Load environment variables on initialization.
+        """
         load_dotenv()
         self.__config = dotenv_values()
 
     def get(self, key: str) -> str:
-        return self.__config[key]
-        
+        """
+        Return the value for the given configuration key.
+        If key is not found, return None.
+        """
+        return self.__config.get(key)
+
     def getAll(self) -> dict:
+        """
+        Return all configuration values.
+        """
         return self.__config
