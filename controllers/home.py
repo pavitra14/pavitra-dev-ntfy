9c33011e8b675b5addaffc1edc39e3da0dc13be5
Pavitra Behre <behrepav@amazon.com>
2023-11-14 17:47:54 +0530

    refactor(Home Controller): Add docstrings and specify return types
    - Add docstrings for the class and all methods
    - Specify return types for all methods

--- controllers/home.py
+++ controllers/home.py
@@ -2,14 +2,22 @@ from fastapi.responses import HTMLResponse
 from controllers.base_controller import BaseController
 from controllers.config import Config
 
-
 class Home(BaseController):
+    """
+    Class representing home controller.
+    """
     def __init__(self, config: Config) -> None:
+        """
+        Initialize home controller.
+        """
         self.__config = config
         self.router.add_api_route("/", self.hello, methods=["GET"])
         self.router.add_api_route("/env", self.getConfig, methods=["GET"])
 
     def hello(self) -> HTMLResponse:
+        """
+        Return a simple HTML page.
+        """
         htmlContent = """
         <html>
             <head>
@@ -23,5 +31,8 @@ class Home(BaseController):
         """
         return HTMLResponse(content=htmlContent, status_code=200)
 
-    def getConfig(self):
+    def getConfig(self) -> dict:
+        """
+        Get all environment configurations.
+        """
         return {"env":self.__config.getAll()}
