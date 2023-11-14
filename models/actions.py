dcdd457c4986725181c36a9881e8a5e71d290b1a
Pavitra Behre <behrepav@amazon.com>
2023-11-14 17:43:58 +0530

    docs(Actions Model): add class docstrings
    - Add docstrings for ActionType and Actions classes

--- models/actions.py
+++ models/actions.py
@@ -2,11 +2,17 @@ from pydantic import BaseModel
 from enum import Enum
 
 class ActionType(Enum):
-    VIEW = "view",
-    BROADCAST = "broadcast",
+    """
+    Enum class for possible action types.
+    """
+    VIEW = "view"
+    BROADCAST = "broadcast"
     HTTP = "http"
 
 class Actions(BaseModel):
+    """
+    BaseModel class for action data.
+    """
     actionType: ActionType
     label: str
     url: str
