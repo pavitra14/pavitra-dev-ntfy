b3dc7e8369e246508777cdc22d4d7b95c781be98
Pavitra Behre <behrepav@amazon.com>
2023-11-14 17:49:44 +0530

    refactor(Notify Controller): Add docstrings and error handling
    - Add docstrings for the class and all methods
    - Add error handling in the `publish` method

--- controllers/notify.py
+++ controllers/notify.py
@@ -1,22 +1,33 @@
-from controllers.config import Config
-from controllers.base_controller import BaseController
-from models.notification import Notification
-from backend.ntfy import ntfy
 from fastapi.responses import ORJSONResponse
 from fastapi.encoders import jsonable_encoder
-
+from controllers.base_controller import BaseController
+from controllers.config import Config
+from models.notification import Notification
+from backend.ntfy import ntfy
 
 class Notify(BaseController):
+    """
+    Class representing the notification controller.
+    """
     def __init__(self, config: Config) -> None:
+        """
+        Initialize Notify controller with configuration.
+        """
         self.__config = config
         self.router.add_api_route("/publish", self.publish, methods=["POST"])
-        
+
     def publish(self, notification: Notification) -> ORJSONResponse:
+        """
+        Publish a notification.
+        """
         channel = ntfy(self.__config)
-        statusCode = channel.send(notification)
-        resp = {
-            "notification": notification, 
-            "headers": notification.getHeaders(),
-            "statusCode" : statusCode
-            }
-        return ORJSONResponse(content=jsonable_encoder(resp), status_code=statusCode)
+        try:
+            statusCode = channel.send(notification)
+            resp = {
+                "notification": notification, 
+                "headers": notification.getHeaders(),
+                "statusCode" : statusCode
+                }
+            return ORJSONResponse(content=jsonable_encoder(resp), status_code=statusCode)
+        except Exception as e:
+            return ORJSONResponse(content={"error": str(e)}, status_code=500)
