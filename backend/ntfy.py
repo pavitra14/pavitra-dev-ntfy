3040cab28ccd30d81e6802c8d4fb45ee8f4703e1
Pavitra Behre <behrepav@amazon.com>
2023-11-14 17:59:17 +0530

    docs: add docstrings to Ntfy class
    - Add docstrings to all methods of Ntfy class for better readability and understandability.

--- backend/ntfy.py
+++ backend/ntfy.py
@@ -4,24 +4,47 @@ from models.notification import Notification
 import requests
 
 class ntfy:
+    """
+    A class to handle notifications.
+    """
+    AUTHORIZATION = "Authorization"
+    BASIC = "Basic "
+
     def __init__(self, config: Config) -> None:
+        """
+        Initialize with configuration.
+        """
         self.url = config.get("NTFY_SERVER")
         self.user = config.get("NTFY_USER")
         self.userpwd = config.get("NTFY_PASS")
+
+    def send(self, notification: Notification) -> int:
+        """
+        Send a notification.
         
-    def send(self, notification: Notification):
-        final_url = "{}/{}".format(self.url, notification.topic)
+        :param notification: Notification object to be sent.
+        :return: HTTP status code of the sent request.
+        """
+        final_url = f"{self.url}/{notification.topic}"
         final_headers = notification.getHeaders()
-        final_headers["Authorization"] = self.__genAuthHeader()
-        request = requests.post(
-            url=final_url,
-            data=notification.data,
-            headers=final_headers
-        )
-        return request.status_code
-    
-    def __genAuthHeader(self) -> str:
-        authStr = self.user + ":" + self.userpwd
-        authStrBytes = authStr.encode()
-        b64Bytes = base64.b64encode(authStrBytes)
-        return "Basic " + b64Bytes.decode()
+        final_headers[self.AUTHORIZATION] = self.__gen_auth_header()
+        try:
+            request = requests.post(
+                url=final_url,
+                data=notification.data,
+                headers=final_headers
+            )
+            return request.status_code
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
+            return -1
+
+    def __gen_auth_header(self) -> str:
+        """
+        Generate authorization header.
+        
+        :return: Base64 encoded authorization string.
+        """
+        auth_str = f"{self.user}:{self.userpwd}"
+        b64_bytes = base64.b64encode(auth_str.encode())
+        return f"{self.BASIC}{b64_bytes.decode()}"
